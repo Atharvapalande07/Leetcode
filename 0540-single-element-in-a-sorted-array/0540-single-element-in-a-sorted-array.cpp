134be8999e43404679aabb239ce94d1f34da0687
Atharvapalande07 <149474955+Atharvapalande07@users.noreply.github.com>
2025-06-20 16:50:08 +0530

    Time: 0 ms (100%), Space: 26.2 MB (67.12%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int> freq;
-        int ans=0;
+        int n = nums.size();
 
-        for(int i=0 ; i<nums.size() ; i++){
-            int num = nums[i];
-            if(freq.find(num) != freq.end()){
-                freq[num]++;
-            }
-            else{
-                freq[num]=1;
-
-            }
-            
-        }
-        for(auto it = freq.begin() ; it != freq.end() ; it++ ){
-            if(it->second == 1){
-                ans= it->first;
-            }
-
-        }
-        return ans;
+        if(n==1) return nums[0];
         
+
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        
+        int start=1;
+        int end = n-2;
+
+         while(start <= end){
+            int mid = start +(end-start)/2;
+ 
+            if(nums[mid] != nums[mid-1]  && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
+
+            if((mid % 2 ==1 && nums[mid] == nums[mid-1]) || (mid%2==0 && nums[mid] == nums[mid+1])){
+                start=mid+1;
+            } 
+            else{
+                end=mid-1;
+            }
+         }
+         return -1;
     }
 };
