51848c8aa9baf735813ec792abf0956294facbcd
Atharvapalande07 <149474955+Atharvapalande07@users.noreply.github.com>
2025-06-30 00:14:25 +0530

    Time: 3 ms (71.02%), Space: 14.9 MB (21.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
         unordered_map<int , int> mp;
+        vector<int> result;
 
         for(int i=0 ;i<nums.size() ; i++){
             int moreneeded=target - nums[i];
