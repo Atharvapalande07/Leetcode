3ea323f11d464fbc646abb661e2a0824ac57bb40
Atharvapalande07 <149474955+Atharvapalande07@users.noreply.github.com>
2025-07-20 00:24:20 +0530

    Time: 0 ms (100%), Space: 95.1 MB (59.74%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        int total = n + m;
-
-        int i=0;
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int i =0;
         int j=0;
+        int n=(n1+n2);
+        int ind2 = n/2;
+        int ind1 = ind2 - 1;
+        int ind1el=-1;
+        int ind2el = -1;
         int cnt=0;
-        int curr =0;
-        int prev = 0;
-
-        while( cnt <= total/2){
-            prev = curr;
-
-            if(i<n && (j>= m || nums1[i] < nums2[j] )){
-                curr =  nums1[i];
+        while(i<n1 && j<n2){
+            if(nums1[i]<nums2[j]){
+                if(cnt == ind1) ind1el=nums1[i];
+                if(cnt == ind2) ind2el=nums1[i];
                 i++;
-            }else{
-                curr = nums2[j];
-                j++;
+                cnt++;
             }
-
-            cnt ++;
-
-
-
+            else{
+                if(cnt == ind1) ind1el=nums2[j];
+                if(cnt == ind2) ind2el=nums2[j];
+                j++;
+                cnt++;
+            }
         }
-        if(total % 2 == 0){
-            return (prev + curr) / 2.0;
-        }else{
-            return curr;
+        while(i<n1){
+            if(cnt == ind1) ind1el=nums1[i];
+            if(cnt == ind2) ind2el=nums1[i];
+            i++;
+            cnt++;
         }
+        while(j<n2){
+            if(cnt == ind1) ind1el=nums2[j];
+            if(cnt == ind2) ind2el=nums2[j];
+            j++;
+            cnt++;
+        }
+        if(n%2==1) return ind2el;
 
-
-        
+        return ((double)ind1el+(double)ind2el)/2.0;
     }
 };
