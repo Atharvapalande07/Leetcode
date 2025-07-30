9777393278ba4a960f2fa9ffcef828c89b799602
Atharvapalande07 <149474955+Atharvapalande07@users.noreply.github.com>
2025-07-30 20:47:39 +0530

    Time: 2 ms (47.03%), Space: 86.1 MB (38.1%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -4,7 +4,7 @@ public:
         int maxval = *max_element(nums.begin() , nums.end());
 
         int count=0;
-        int maxlen=0;
+        int maxlen=1;
 
         for(int i=0 ; i<nums.size() ; i++){
             if(nums[i] == maxval){
