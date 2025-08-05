1469fa687026e90f444375d98bbcc99347c85b6e
Atharvapalande07 <149474955+Atharvapalande07@users.noreply.github.com>
2025-08-05 20:15:45 +0530

    Time: 38 ms (66.18%), Space: 74.9 MB (77.39%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,50 +1,25 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        // brute force try to make every single index as a starting point
+        unordered_map<int, int> freq;
         int n = fruits.size();
-        if(n==1 || n==2)
-        return n;
-        // int ans = INT_MIN;
-        // for (int i = 0; i < n-1; i++) {
-        //     unordered_set<int> st;
-        //     st.insert(fruits[i]);
-        //     st.insert(fruits[i + 1]);
-        //     int count = 2;
-        //     for (int j = i + 2; j < n; j++) {
-        //         int ele = fruits[j];
-        //         if (st.size() == 1 || st.find(ele) != st.end()) {
-        //             st.insert(ele);
-        //             count++;
-        //         } else
-        //             break;
-        //     }
-        //     ans = max(ans, count);
-        // }
-        // return ans;
-
-        // now using optimised O(N) sliding window two pointer approach
 
         int i = 0;
-        int j = 0;
-        int ans = INT_MIN;
-        int count = 0;
-        unordered_map<int,int> mp;
-        while(i<n && j<n){
-            mp[fruits[j]]++;
-            while(mp.size() > 2){
-                // shrink the window
-                mp[fruits[i]]--;
-                if(mp[fruits[i]]==0)
-                mp.erase(fruits[i]);
+        int maxlen = 0;
+
+        for (int j = 0; j < n; j++) {
+            freq[fruits[j]]++;
+
+            while (freq.size() > 2) {
+                freq[fruits[i]]--;
+
+                if (freq[fruits[i]] == 0) {
+                    freq.erase(fruits[i]);
+                }
                 i++;
-                count--;
             }
-            count++;
-            // update the answer also
-            ans = max(ans,count);
-            j++;
+            maxlen = max(maxlen, j - i + 1);
         }
-        return ans;
+        return maxlen;
     }
 };
